ac04d256e50c80ab024ae9f98f9a01a480c901d1
nasre <nasreensheik365@gmIil.com>
2023-09-27 14:45:10 +0530

    code1

--- 01.frontend.sh
+++ 01.frontend.sh
@@ -6,6 +6,7 @@ echo -e "\e[31m Nginx server installed successfully\e[0m"
  echo -e "\e[31m Nginx server started and enabled successfully\e[0m"
  cd /usr/share/nginx/html
  rm -rf *
+ echo -e "\e[31m Default server content removed\e[0m"
  curl -O https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
  unzip frontend.zip
  rm -rf frontend.zip
