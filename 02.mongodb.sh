c3cc81d468e2013bff9cd1596dcf9763d980ef1f
nasre <nasreensheik365@gmIil.com>
2023-09-28 10:41:14 +0530

    code1

--- 02.mongodb.sh
+++ 02.mongodb.sh
@@ -3,7 +3,7 @@ cp /root/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo
 echo -e "\e[32m Repo file downloaded successfully\e[0m"
 yum install mongodb-org -y
 echo -e "\e[32m mongodb installed successfully \e[0m"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/momgod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 echo -e "\e[32m mongodb listen address changed successfully \e[0m"
 systemctl start mongod
 systemctl enable mongod
