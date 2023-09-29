9b4d9dd982ffebe17d8a1255b0e8efefbe3d160e
nasre <nasreensheik365@gmIil.com>
2023-09-29 14:08:44 +0530

    code1

--- 01.frontend.sh
+++ 01.frontend.sh
@@ -1,14 +1,14 @@
 echo -e "\e[33m Installing Nginx Server\e[0m"
-yum install nginx -y
+yum install nginx -y &>>/tmp/frontend.log
 echo -e "\e[33m Enabling and starting the Nginx server\e[0m"
-systecmtl enable nginx
+systecmtl enable nginx &>>/tmp/frontend.log
 systemctl start nginx
 echo -e "\e[33m Removing default Nginx content\e[0m"
 cd /usr/share/nginx/html
-rm -rf *
+rm -rf * &>>/tmp/frontend.log
 echo -e "\e[33m Download New Content to Nginx\e[0m"
-curl -O https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-unzip frontend.zip
+curl -O https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/frontend.log
+unzip frontend.zip &>>/tmp/frontend.log
 rm -rf frontend.zip
 
 
